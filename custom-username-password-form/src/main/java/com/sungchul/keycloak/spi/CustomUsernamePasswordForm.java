f75b3e0685fdb141bbb0e2ca7b29f1fd40043ad8
kkimsungchul <kimsc1218@gmail.com>
2022-10-17 10:10:23 +0900

    2022.10.17
    커스텀 키클락 로그인 파일 수정

--- custom-username-password-form/src/main/java/com/sungchul/keycloak/spi/CustomUsernamePasswordForm.java
+++ custom-username-password-form/src/main/java/com/sungchul/keycloak/spi/CustomUsernamePasswordForm.java
@@ -6,6 +6,7 @@ import com.nimbusds.jose.crypto.RSADecrypter;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.specimpl.MultivaluedMapImpl;
 import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.AuthenticationFlowError;
 import org.keycloak.authentication.authenticators.browser.UsernamePasswordForm;
 import org.keycloak.crypto.Algorithm;
 import org.keycloak.crypto.KeyUse;
@@ -33,10 +34,25 @@ public class CustomUsernamePasswordForm extends UsernamePasswordForm {
     @Override
     public void action(AuthenticationFlowContext context) {
         MultivaluedMap<String, String> formData = context.getHttpRequest().getDecodedFormParameters();
-        transformPassword(context.getSession(),context.getRealm(),formData,context);
+        boolean timeCheck = transformPassword(context.getSession(),context.getRealm(),formData,context);
         //화면단에 데이터 전송
         //꺼내서 사용할때는  <input type="hidden" id="bb" value ="${(testData!'')}"> 이렇게 사용하면 됨
-        context.form().setAttribute("testData","sample test data");
+        //context.form().setAttribute("testData","sample test data");
+
+        //클라이언트와 서버의 시간차이가 5분이상일경우(클라이언트가 5분 더 느릴경우) 오류 처리
+        if(!timeCheck){
+            context.failureChallenge(AuthenticationFlowError.INTERNAL_ERROR,
+                    context.form().setError("서버와 클라이언트의 시간차이가 5분이상일 경우 로그인이 제한됩니다.","Timestamp is to far in the past").createErrorPage(
+                            Response.Status.INTERNAL_SERVER_ERROR
+                    ));
+
+
+
+//            Response challengeResponse = challenge(context,"서버와 클라이언트의 시간차이가 5분이상일 경우 로그인이 제한됩니다.", "loginTimeout");
+//            context.challenge(challengeResponse);
+
+            return;
+        }
 
         if (formData.containsKey("cancel")) {
             context.cancelLogin();
@@ -115,7 +131,7 @@ public class CustomUsernamePasswordForm extends UsernamePasswordForm {
 
     }
 
-    private void transformPassword(KeycloakSession session, RealmModel realm, MultivaluedMap<String, String> formData,AuthenticationFlowContext context) throws IllegalStateException {
+    private boolean transformPassword(KeycloakSession session, RealmModel realm, MultivaluedMap<String, String> formData,AuthenticationFlowContext context) throws IllegalStateException {
         // Get the default active RSA key
         KeyWrapper activeRsaKey = session.keys().getActiveKey(realm, KeyUse.SIG, Algorithm.RS256);
         // read Password from input data
@@ -123,20 +139,25 @@ public class CustomUsernamePasswordForm extends UsernamePasswordForm {
         JWEObject jweObject = parseJweObject(activeRsaKey, passwordJWE);
         Map<String, Object> jsonObject = jweObject.getPayload().toJSONObject();
         // Validate timestamp, make sure time is not far in the pass.
-        validateTimeout(jsonObject,context);
+        boolean timeCheck =validateTimeout(jsonObject,context);
         // Set cleartext password in inputData
         formData.addFirst("password",(String) jsonObject.get("pwd"));
+        return timeCheck;
     }
 
-    private void validateTimeout(Map<String, Object> jsonObject,AuthenticationFlowContext context) {
+    private boolean validateTimeout(Map<String, Object> jsonObject,AuthenticationFlowContext context) {
         ZonedDateTime dateTime = ZonedDateTime.parse((String) jsonObject.get("timestamp"));
         ZonedDateTime now = ZonedDateTime.now();
+
+        //if(true){
         if (ChronoUnit.MINUTES.between(dateTime, now) > 5) {
             logger.warn("Timestamp is to far in the past.");
             //throw new IllegalStateException("Timestamp is to far in the past.");
             context.form().setAttribute("clientTimeError","clientTimeError");
+            return false;
         }
-        context.form().setAttribute("testData","zxcvz1111");
+
+        return true;
     }
 
     private JWEObject parseJweObject(KeyWrapper activeRsaKey, String passwordJWE) {
